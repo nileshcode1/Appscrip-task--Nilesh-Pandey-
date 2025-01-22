180435f64079315eca1c28326da031f442e9c2d1
nileshcode1 <itsofficialcode@gmail.com>
2025-01-22 23:36:52 +0530

    site_url change

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 export default {
-    siteUrl: process.env.SITE_URL || 'https://appscrp-ecom.vercel.app/', // Replace with your actual domain
-   
-  }
+  siteUrl:
+    process.env.SITE_URL ||
+    "https://679131ee46d8da00c67d970e--roaring-moxie-154441.netlify.app/", 
+};
